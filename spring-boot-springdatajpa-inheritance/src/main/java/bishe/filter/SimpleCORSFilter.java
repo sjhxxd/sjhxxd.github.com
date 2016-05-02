d00fd7814081aab7761d26277694e32bfb233d41
sjhxxd <sjhxxd@163.com>
2016-05-02 15:24:49 +0800

    解决跨域

--- spring-boot-springdatajpa-inheritance/src/main/java/bishe/filter/SimpleCORSFilter.java
+++ spring-boot-springdatajpa-inheritance/src/main/java/bishe/filter/SimpleCORSFilter.java
@@ -16,7 +16,8 @@ public class SimpleCORSFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Methods",
 				"POST, GET, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 		chain.doFilter(req, res);
 	}
 
